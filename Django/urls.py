173ebad261b02dadcdce8ebc29b77fbe9c2cbd6a
KauanNaves <kauannazanaves0@gmail.com>
2025-12-06 16:36:46 -0300

    Entendendo HttpRequest e HttpResponse

--- Django/urls.py
+++ Django/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse('Hello, your request was successful!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('my-view/', my_view),
 ]
